f57c97af9e6fcf9907e69fd72aaa91d3d323ffce
hokuto06 <emartins.ecnex@gmail.com>
2025-05-28 18:55:15 -0300

    Subiendo blog a producción

---
+++ django-api/django_api_blog/urls.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from categories.api.router import router_categories
+from posts.api.router import router_posts
+from comments.api.router import router_comments
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="BLOG API",
+      default_version='v1',
+      description="Documentación del BLOG",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@estebanmartins.com.ar"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('users.api.router')),
+    path('api/', include(router_categories.urls)),
+    path('api/', include(router_posts.urls)),
+    path('api/', include(router_comments.urls))
+]
