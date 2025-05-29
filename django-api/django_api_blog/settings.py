f98c95f20b334e292e8277eb50eeda115befea4f
hokuto06 <emartins.ecnex@gmail.com>
2025-05-29 02:20:07 -0300

    Permitir conexiones desde cualquier origin dj-settings

--- django-api/django_api_blog/settings.py
+++ django-api/django_api_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$11xzf&!@w@m4!^tohc+xx1102qyjh7we&#)&t(#i)(=*y5g*k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
