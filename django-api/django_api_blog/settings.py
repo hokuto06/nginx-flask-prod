b218d024e5c5a38c9e708836e009cc44a039e8fb
hokuto06 <emartins.ecnex@gmail.com>
2025-05-30 15:34:01 -0300

    settings: Debug false

--- django-api/django_api_blog/settings.py
+++ django-api/django_api_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$11xzf&!@w@m4!^tohc+xx1102qyjh7we&#)&t(#i)(=*y5g*k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
