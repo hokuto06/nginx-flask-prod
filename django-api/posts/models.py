a5de88b6a27bf27ca97d5b957b2e7becaaf015cd
hokuto06 <emartins.ecnex@gmail.com>
2025-05-31 01:03:04 -0300

    Blog/ migraciones posts-model-description y post.html

--- django-api/posts/models.py
+++ django-api/posts/models.py
@@ -6,6 +6,7 @@ from categories.models import Category
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    description = models.CharField(max_length=500, blank=True, null=True)
     slug = models.SlugField(max_length=255, unique=True)
     miniature = models.ImageField(upload_to='posts/img/')
     created_at = models.DateTimeField(auto_now_add=True)
