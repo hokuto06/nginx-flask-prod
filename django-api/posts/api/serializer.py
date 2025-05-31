3b9bc4ecc3fb17f6b2ae9b0dc0129bb83d85154d
hokuto06 <emartins.ecnex@gmail.com>
2025-05-31 04:36:26 -0300

    cambios en blog

--- django-api/posts/api/serializer.py
+++ django-api/posts/api/serializer.py
@@ -7,8 +7,10 @@ from categories.api.serializers import CategorySerializer
 class PostSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     category = CategorySerializer()
+    miniature = serializers.SerializerMethodField()
     class Meta:
         model = Post
         fields = ['title','content','slug','miniature','created_at','description','published','user','category']
-
+    def get_miniature(self, obj):
+        return obj.miniature.url  # => /media/posts/img/portada_ldin.png
         
